c1585080377e8dd55d4410a7ea27990aa0921353
Phil Varner <philvarner@gmail.com>
2021-09-03 10:58:30 -0400

    add todo

--- deployment/cdk/app.py
+++ deployment/cdk/app.py
@@ -79,6 +79,7 @@ class MmtPipelineStack(Stack):
                     iam.PolicyStatement(
                         actions=["sts:AssumeRole"],
                         resources=["*"],
+                        # todo: re-add
                         # conditions={
                         #     "StringEquals": {'iam:ResourceTag/aws-cdk:bootstrap-role': 'deploy'}
                         # },
